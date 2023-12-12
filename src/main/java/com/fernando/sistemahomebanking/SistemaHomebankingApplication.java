223b440a8e6f8865fcc049b726a451c7257d98e2
Fernando Gamarra <ferpg1993@gmail.com>
2023-12-12 17:18:39 -0300

    first commit

---
+++ src/main/java/com/fernando/sistemahomebanking/SistemaHomebankingApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.sistemahomebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaHomebankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaHomebankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernando/sistemahomebanking/SistemaHomebankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.sistemahomebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaHomebankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
